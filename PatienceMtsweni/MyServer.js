de4e4fcdec34c1e8c2551b121c80b61ebf4437d4
PATIENCE MTSWENI <u11116774@tuks.co.za>
2015-03-20 09:07:13 +0200

    added code to determine if file has a mimetype

--- PatienceMtsweni/MyServer.js
+++ PatienceMtsweni/MyServer.js
@@ -4,28 +4,42 @@
     /** Define dependencies*/
 var express = require("express");
 var multer = require('multer');
+//var mime = require("mime");
 var app = express();
 var done = false;
+var isValidMime;
+var mimeType;
+
 /** Configure the multer. */
 app.use(multer({dest: './uploads/',
     rename: function(fieldname, filename){
         return filename+Date.now();
     },
     onFileUploadComplete:function(file){
+        mimeType = file.mimetype;
+        console.log(mimeType);
         done = true;
     }
 }));
-
+/** checks if mimetype is valid */
+if(mimeType != "undefined")
+{
+    isValidMime = true;
+}
 /*Handling the routes. */
 app.get('/', function(req, res){
-        res.sendFile("upload.html", {root:__dirname})
+    res.sendFile("upload.html", {root:__dirname})
 });
 
 app.post('/api/upload', function(req, res){
-    if(done==true) {
+   if(done==true && isValidMime == true) {
        console.log(req.files);
         res.end("File uploaded");
     }
+    else
+   {
+       res.end("File not uploaded")
+   }
 });
 
 /*Run the server. */
